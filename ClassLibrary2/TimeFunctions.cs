a1eca5a2501fa79dbfb0c3ff4011d21790c46e7c
Cristos Vasilas <cvasilas@swapa.org>
2019-05-22 11:30:14 -0500

    A Work in progress

--- ClassLibrary2/TimeFunctions.cs
+++ ClassLibrary2/TimeFunctions.cs
@@ -27,20 +27,20 @@ public class TimeFunctions
         {
             conn.Open();
             SqlCommand cmd = new SqlCommand(
-                "select count (*) from IATA_TimeZone", conn);
+                "select count (*) from [dbo].[IATA_TimeZone]", conn);
             return (int)cmd.ExecuteScalar();
         }
     }
 
  
-    private static string TimeZoneForIATA(string iata)
+    public static string TimeZoneForIATA(string iata)
     {
         using (SqlConnection conn
             = new SqlConnection("context connection=true"))
         {
             conn.Open();
             SqlCommand cmd = new SqlCommand(
-                "select TimeZone from IATA_TimeZone where iata = '" + iata + "'", conn);
+                "select TimeZone from [dbo].[IATA_TimeZone] where iata = '" + iata + "'", conn);
             return (string)cmd.ExecuteScalar();
         }
     }
@@ -48,9 +48,26 @@ public class TimeFunctions
     [SqlFunction(DataAccess = DataAccessKind.Read)]
     public static DateTime HerbToLocal(DateTime date, int time, string iata)
     {
-        string timeZone = TimeZoneForIATA(iata);
+        // Uses TZConvert Nugat Library
+        // https://github.com/mj1856/TimeZoneConverter
+        // PM> Install-Package TimeZoneConverter
 
-        return date.AddDays(5);
+        // Station Time Zone
+        string timeZoneIATA = TimeZoneForIATA(iata);
+        string tzString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneIATA);
+        TimeZoneInfo tzStation = TimeZoneInfo.FindSystemTimeZoneById(tzString);
+
+        // Local Time Zone
+        string localtzString = TimeZoneConverter.TZConvert.IanaToWindows("America/Chicago");
+        TimeZoneInfo tzLocal = TimeZoneInfo.FindSystemTimeZoneById(localtzString);
+
+        var hour = time / 100;
+        var minutes = time - (hour * 100);
+        var newDate = new DateTime(date.Year, date.Month, date.Day, hour, minutes, 0);
+
+        var convertedDate = TimeZoneInfo.ConvertTime(newDate, tzLocal, tzStation);
+        // Convert from Central Time to Local Time
+        return convertedDate;
     }
 
 }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Uses TZConvert Nugat Library
+            // https://github.com/mj1856/TimeZoneConverter
+            // PM> Install-Package TimeZoneConverter
+
+            var time = 400;
+            DateTime date = DateTime.Now;
+
+            // Station Time Zone
+            string timeZoneIATA = "America/Denver";
+            string tzString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneIATA);
+            TimeZoneInfo tzStation = TimeZoneInfo.FindSystemTimeZoneById(tzString);
+
+            // Local Time Zone
+           string localtzString = TimeZoneConverter.TZConvert.IanaToWindows("America/Chicago");
+           TimeZoneInfo tzLocal = TimeZoneInfo.FindSystemTimeZoneById(localtzString);
+
+           var hour = time / 100;
+           var minutes = time - (hour * 100);
+           var newDate = new DateTime(date.Year, date.Month, date.Day, hour, minutes, 0);
+
+           // Convert from Central Time to Local Time
+           TimeZoneInfo.ConvertTime(newDate, tzLocal, tzStation);
+
+           Console.WriteLine(TimeZoneInfo.ConvertTime(newDate, tzLocal, tzStation));
+
+            Console.ReadLine();
+          
+        }
+    }
+}
