47e871312579b5dc8c11509229a99ab9124b83f6
Cristos Vasilas <cvasilas@swapa.org>
2019-05-23 15:01:18 -0500

    TimeFunctions and Console test app complete

--- ClassLibrary2/TimeFunctions.cs
+++ ClassLibrary2/TimeFunctions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Data;
 using Microsoft.SqlServer.Server;
-using System.Data.SqlTypes;
 using System.Data.SqlClient;
 
 public class TimeFunctions
@@ -18,10 +16,17 @@ public class TimeFunctions
     //{
     //    return i + 1;
     //}
-    private static string TimeZoneForIATA(string stationIATA)
+
+    //"Server=tcp:az-sqlpsrc01.int.swapa.org ,1433;Database=Scheduling_Dev;uid=MobileApp;pwd=fzmnfAmAox61;"
+    public static string TimeZoneForIATA(string stationIATA)
     {
-        using (SqlConnection conn = new SqlConnection("context connection=true"))
+        using (SqlConnection conn = new SqlConnection())
         {
+            conn.ConnectionString =
+                "Data Source=tcp:az-sqlpsrc01.int.swapa.org;" +
+                "Initial Catalog=Scheduling_Dev;" +
+                "User id=MobileApp;" +
+                "Password=fzmnfAmAox61;";
             conn.Open();
             SqlCommand cmd = new SqlCommand(
                 "select TimeZone from [dbo].[IATA_TimeZone] where iata = '" + stationIATA + "'", conn);
@@ -50,11 +55,16 @@ public class TimeFunctions
          
          */
     [SqlFunction(DataAccess = DataAccessKind.Read)]
-    public static DateTime HerbToLocal(DateTime date, int time, string stationIATA)
+    public static DateTime HerbDateandTimeToStationTime(DateTime date, int time, string stationIATA)
     {
         // Uses TZConvert Nugat Library
         // https://github.com/mj1856/TimeZoneConverter
         // PM> Install-Package TimeZoneConverter
+
+        // Convert integer time to create a dateTime
+        var hour = time / 100;
+        var minutes = time - (hour * 100);
+        var herbDateTime = new DateTime(date.Year, date.Month, date.Day, hour, minutes, 0);
 
         // Station Time Zone
         string timeZoneStationIATA = TimeZoneForIATA(stationIATA);
@@ -66,12 +76,106 @@ public class TimeFunctions
         string tzHerbString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneHerbIATA);
         TimeZoneInfo tzHerb = TimeZoneInfo.FindSystemTimeZoneById(tzHerbString);
 
+        // Convert from Central Time to Local/station Time
+        var stationDateTime = TimeZoneInfo.ConvertTime(herbDateTime, tzHerb, tzStation);
+        
+        return stationDateTime;
+    }
+
+    [SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static DateTime StationTimeToStationTime(DateTime dateTime, string fromStationIATA, string toStationIATA)
+    {
+        // Uses TZConvert Nugat Library
+        // https://github.com/mj1856/TimeZoneConverter
+        // PM> Install-Package TimeZoneConverter
+
+        var fromStationDateTime = dateTime;
+
+        // Station 1 Time Zone
+        string timeZoneFromStationIATA = TimeZoneForIATA(fromStationIATA);
+        string tzFromStationString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneFromStationIATA);
+        TimeZoneInfo tzFromStation = TimeZoneInfo.FindSystemTimeZoneById(tzFromStationString);
+
+        // Station 2 Time Zone
+        string timeZoneToStationIATA = TimeZoneForIATA(toStationIATA);
+        string tzToStationString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneToStationIATA);
+        TimeZoneInfo tzToStation = TimeZoneInfo.FindSystemTimeZoneById(tzToStationString);
+
+        // Convert from Central Time to Local/station Time
+        var stationDateTime = TimeZoneInfo.ConvertTime(fromStationDateTime, tzFromStation, tzToStation);
+
+        return stationDateTime;
+    }
+
+    [SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static DateTime HerbDateandTimeToUTCTime(DateTime date, int time)
+    {
+        // Uses TZConvert Nugat Library
+        // https://github.com/mj1856/TimeZoneConverter
+        // PM> Install-Package TimeZoneConverter
+
+        // Convert integer time to create a dateTime
         var hour = time / 100;
         var minutes = time - (hour * 100);
         var herbDateTime = new DateTime(date.Year, date.Month, date.Day, hour, minutes, 0);
 
-        var stationDateTime = TimeZoneInfo.ConvertTime(herbDateTime, tzHerb, tzStation);
-        // Convert from Central Time to Local Time
+        // UTC Time Zone
+        TimeZoneInfo tzUTC = TimeZoneInfo.Utc;
+
+        // Central Time Zone
+        string timeZoneHerbIATA = TimeZoneForIATA("DAL");
+        string tzHerbString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneHerbIATA);
+        TimeZoneInfo tzHerb = TimeZoneInfo.FindSystemTimeZoneById(tzHerbString);
+
+        // Convert from Central Time to Local/station Time
+        var UTCDateTime = TimeZoneInfo.ConvertTime(herbDateTime, tzHerb, tzUTC);
+
+        return UTCDateTime;
+    }
+
+    [SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static DateTime StationTimeToUTC(DateTime dateTime, string stationIATA)
+    {
+        // Uses TZConvert Nugat Library
+        // https://github.com/mj1856/TimeZoneConverter
+        // PM> Install-Package TimeZoneConverter
+
+        var stationDateTime = dateTime;
+
+        // UTC Time Zone
+        TimeZoneInfo tzUTC = TimeZoneInfo.Utc;
+
+        // Station Time Zone
+        string timeZoneStationIATA = TimeZoneForIATA(stationIATA);
+        string tzStationString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneStationIATA);
+        TimeZoneInfo tzStation = TimeZoneInfo.FindSystemTimeZoneById(tzStationString);
+
+        // Convert from Station Time to UTC Time
+        var UTCDateTime = TimeZoneInfo.ConvertTime(stationDateTime, tzStation, tzUTC);
+
+        return UTCDateTime;
+    }
+
+    [SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static DateTime UTCtoStationTime(DateTime dateTime, string stationIATA)
+    {
+        // Uses TZConvert Nugat Library
+        // https://github.com/mj1856/TimeZoneConverter
+        // PM> Install-Package TimeZoneConverter
+
+        var utcDateTime = dateTime;
+
+        // UTC Time Zone
+        TimeZoneInfo tzUTC = TimeZoneInfo.Utc;
+
+        // Station Time Zone
+        string timeZoneStationIATA = TimeZoneForIATA(stationIATA);
+        string tzStationString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneStationIATA);
+        TimeZoneInfo tzStation = TimeZoneInfo.FindSystemTimeZoneById(tzStationString);
+
+        // Convert from Station Time to UTC Time
+        var stationDateTime = TimeZoneInfo.ConvertTime(utcDateTime, tzUTC, tzStation);
+
         return stationDateTime;
     }
 
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
 
             // Uses TZConvert Nugat Library
             // https://github.com/mj1856/TimeZoneConverter
@@ -14,30 +14,30 @@ namespace ConsoleApp1
 
             var time = 400;
             DateTime date = new DateTime(2019, 5, 01);
+            var hour = time / 100;
+            var minutes = time - (hour * 100);
+            var originalTime = new DateTime(date.Year, date.Month, date.Day, hour, minutes, 0);
 
-            // Station Time Zone
-            string timeZoneIATA = "America/Phoenix";
-            string tzString = TimeZoneConverter.TZConvert.IanaToWindows(timeZoneIATA);
-            TimeZoneInfo tzStation = TimeZoneInfo.FindSystemTimeZoneById(tzString);
+            var stationTime = TimeFunctions.HerbDateandTimeToStationTime(date, time, "LAX");
+            var utcTime = TimeFunctions.StationTimeToUTC(stationTime, "LAX");
+            var bwiTime = TimeFunctions.UTCtoStationTime(utcTime, "BWI");
+            var laxTime = TimeFunctions.StationTimeToStationTime(bwiTime, "BWI", "LAX");
+            var herbTime = TimeFunctions.StationTimeToStationTime(laxTime, "LAX", "DAL");
+            var phxTime = TimeFunctions.StationTimeToStationTime(herbTime, "DAL", "PHX");
 
-            // Local Time Zone
-           string localtzString = TimeZoneConverter.TZConvert.IanaToWindows("America/Chicago");
-           TimeZoneInfo tzCentral = TimeZoneInfo.FindSystemTimeZoneById(localtzString);
-
-           var hour = time / 100;
-           var minutes = time - (hour * 100);
-           var newDate = new DateTime(date.Year, date.Month, date.Day, hour, minutes, 0);
-
-           // Convert from Central Time to Local Time
-           var convertedDate = TimeZoneInfo.ConvertTime(newDate, tzCentral, tzStation);
-
+            Console.WriteLine(
+                "originalTime HerbTime: {0}\nLAX_Time: {1}\nutcTime: {2}\nBWI_Time: {3}\nLAX_Time:{4}\nHerbTime:{5}\nPHX_Time:{6}", originalTime, stationTime, utcTime, bwiTime, laxTime, herbTime, phxTime
+                );
            
-           Console.WriteLine("{0} {1} is {2} {3}.",
-           convertedDate, tzStation.IsDaylightSavingTime(newDate) ? tzStation.DaylightName : tzStation.StandardName,
-           newDate, tzCentral.IsDaylightSavingTime(newDate) ? tzCentral.DaylightName : tzCentral.StandardName);
+           //Console.WriteLine("{0} {1} is {2} {3}.",
+           //convertedDate, tzStation.IsDaylightSavingTime(newDate) ? tzStation.DaylightName : tzStation.StandardName,
+           //newDate, tzCentral.IsDaylightSavingTime(newDate) ? tzCentral.DaylightName : tzCentral.StandardName);
 
             Console.ReadLine();
           
         }
     }
+
+ 
 }
+
